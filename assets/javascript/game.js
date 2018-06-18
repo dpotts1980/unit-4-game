bdd826e5c1d616b9ad1b21b6b00d06052195fee8
Daniel Potts <drp777@hotmail.com>
2018-06-18 07:45:25 -0500

    homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,14 +14,14 @@
 
    //this is where we'll create the overall random number//
    var randomResult = Math.floor((Math.random() * 101) + 19);
-   console.log(randomResult);
+   //console.log(randomResult);
    
    //this is a for loop to create the four separate crystals//
-    $("#randomResult").html("Number to Get: ");
+    $("#randomResult").html("Number to Get: " + randomResult);
    for(i = 0; i < 4; i++) {
 
     //this is where we have the random number between 1-12 for each crystal//
-    var random = Math.floor(Math.random() * 12)
+    var random = Math.floor((Math.random() * 11) + 1);
        //console.log(random);
        var crystal = $("<div>");
            crystal.attr({
@@ -30,3 +30,13 @@
            });
        $(".crystals").append(crystal);
    }
+
+   //this is where we'll create our on click for each crystal//
+   $(".crystal").on("click", function() {
+    
+    var num = parseInt($(this).attr("data-random"));
+
+    var result = num + 5;
+    console.log(num, ' + ', result);
+
+   });
