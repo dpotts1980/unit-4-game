71719e37720366a36d0d1ea645a99284ee9beb1d
Daniel Potts <drp777@hotmail.com>
2018-06-18 07:04:34 -0500

    homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,43 +5,28 @@
    4. When clicking any crystal it should be adding with the previous result.
    5. Until it === the exact same score... meaning WE WIN!!
    6. If our score is > then the random number WE LOSE :(
-   7. We will increment wins++ or losses++;
+   7. We will increment wins++ or losses++
+   */
+  
+   var randomResult;
+   var wins;
+   var losses;
 
-//we'll set the doc to be ready*/
+   //this is where we'll create the overall random number//
+   var randomResult = Math.floor((Math.random() * 101) + 19);
+   console.log(randomResult);
+   
+   //this is a for loop to create the four separate crystals//
+    $("#randomResult").html("Number to Get: ");
+   for(i = 0; i < 4; i++) {
 
-$(document).ready(function()
-
-//Before anything we need to set some variables*/
-
-
-//This is  where we grab the numberToMatch element//
-/*This is where we enter the random number generator ranging from 19-120*/
-{
-    var Random=Math.floor(Math.random()*101+19)
-    // Selects a random number to be shown at the start of the game
-    // Number should be should be between 19 - 120
-    //
-    $('#randomNumber').text(Random);
-    // Appending random number to the randomNumber id in the html doc
-    //
-    
-
-
-//This is where we enter the 4 crystals and attach an on.click method to them//
-
-
-//we will also create a random number generator for each crystal ranging from 1-12//
-
-//this is where we have the userguess counter//
-
-
-//this is where we have if/else when the user === the computerNumber or goes over//
-
-
-
-
-
-//we will create a counter to measure wins + and losses//
-
-
-//we need to figure out how to reset the game as well//
+    //this is where we have the random number between 1-12 for each crystal//
+    var random = Math.floor(Math.random() * 12)
+       //console.log(random);
+       var crystal = $("<div>");
+           crystal.attr({
+            "class": 'crystal',
+            "data-random": random
+           });
+       $(".crystals").append(crystal);
+   }
